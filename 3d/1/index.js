cf998d4545bf339b20600c219c9e01f9cb6f0104
piyoppi <garakuta.toolbox@gmail.com>
2020-11-15 18:30:31 +0900

    箱を描画してみる

--- 3d/1/index.js
+++ 3d/1/index.js
@@ -1,17 +1,21 @@
 const vertexShaderSource = `
   attribute vec4 a_position;
+  attribute vec4 a_color;
   uniform mat4 u_matrix;
+  varying vec4 v_color;
 
   void main() {
     gl_Position = u_matrix * a_position;
+    v_color = a_color;
   }
 `;
 
 const fragmentShaderSource = `
   precision mediump float;
+  varying vec4 v_color;
 
   void main() {
-    gl_FragColor = vec4(1, 0, 0, 1);
+    gl_FragColor = v_color;
   }
 `;
 
@@ -151,15 +155,18 @@ class Plane {
     this.width = width;
     this.height = height;
     this.vertexes = [];
+    this.setXYPlaneVertexes();
   }
 
   setXYPlaneVertexes() {
-    const [x1, y1] = [this.x, this.y];
-    const x2 = this.x + this.width;
-    const y2 = this.y + this.height;
-    const z = this.z;
+    this.vertexes = Plane.xyPlaneVertexes(this.x, this.y, this.z, this.width, this.height);
+  }
 
-    this.vertexes = [
+  static xyPlaneVertexes(x1, y1, z, width, height) {
+    const x2 = x1 + width;
+    const y2 = y1 + height;
+
+    return [
       x1, y1, z,
       x2, y1, z,
       x1, y2, z,
@@ -169,8 +176,103 @@ class Plane {
     ];
   }
 
-  initialize() {
-    this.setXYPlaneVertexes();
+  static zyPlaneVertexes(x, y1, z1, height, depth) {
+    const y2 = y1 + height;
+    const z2 = z1 + depth;
+
+    return [
+      x, y1, z1,
+      x, y1, z2,
+      x, y2, z1,
+      x, y2, z1,
+      x, y1, z2,
+      x, y2, z2
+    ];
+  }
+
+  static xzPlaneVertexes(x1, y, z1, width, depth) {
+    const x2 = x1 + width;
+    const z2 = z1 + depth;
+
+    return [
+      x1, y, z1,
+      x2, y, z1,
+      x1, y, z2,
+      x1, y, z2,
+      x2, y, z1,
+      x2, y, z2
+    ];
+  }
+}
+
+class Cube {
+  constructor(x, y, z, width, height, depth) {
+    this.x = x;
+    this.y = y;
+    this.z = z;
+    this.width = width;
+    this.height = height;
+    this.depth = depth;
+    this.vertexes = [];
+    this.vertexColors = [];
+
+    this.setVertexes();
+    this.setColors();
+  }
+
+  setVertexes() {
+    const center = [
+      this.width / 2.0,
+      this.height / 2.0,
+      this.depth / 2.0,
+    ];
+
+    this.vertexes = [
+      ...Plane.xyPlaneVertexes(this.x - center[0], this.y - center[1], this.z - center[2], this.width, this.height),
+      ...Plane.zyPlaneVertexes(this.x + center[0], this.y - center[1], this.z - center[2], this.height, this.depth),
+      ...Plane.xyPlaneVertexes(this.x - center[0], this.y - center[1], this.z + center[2], this.width, this.height),
+      ...Plane.zyPlaneVertexes(this.x - center[0], this.y - center[1], this.z - center[2], this.height, this.depth),
+      ...Plane.xzPlaneVertexes(this.x - center[0], this.y - center[1], this.z - center[2], this.width, this.depth),
+    ]
+  }
+
+  setColors() {
+    this.vertexColors = [
+      255, 0, 0, 255,
+      255, 0, 0, 255,
+      255, 0, 0, 255,
+      255, 0, 0, 255,
+      255, 0, 0, 255,
+      255, 0, 0, 255,
+
+      0, 255, 0, 255,
+      0, 255, 0, 255,
+      0, 255, 0, 255,
+      0, 255, 0, 255,
+      0, 255, 0, 255,
+      0, 255, 0, 255,
+
+      0, 0, 255, 255,
+      0, 0, 255, 255,
+      0, 0, 255, 255,
+      0, 0, 255, 255,
+      0, 0, 255, 255,
+      0, 0, 255, 255,
+
+      0, 255, 255, 255,
+      0, 255, 255, 255,
+      0, 255, 255, 255,
+      0, 255, 255, 255,
+      0, 255, 255, 255,
+      0, 255, 255, 255,
+
+      255, 0, 0, 255,
+      255, 0, 0, 255,
+      255, 0, 0, 255,
+      255, 0, 0, 255,
+      255, 0, 0, 255,
+      255, 0, 0, 255,
+    ];
   }
 }
 
@@ -181,10 +283,11 @@ class Camera {
     this.height = height;
   }
 
-  setTransformMatrix(pos, deg, scale) {
-    const rad = (deg / 180.0) * 2.0 * Math.PI;
+  setTransformMatrix(pos, rot, scale) {
+    const rad = rot.map( deg => (deg / 180.0) * 2.0 * Math.PI );
     const cos = Math.cos(rad);
     const sin = Math.sin(rad);
+    const depth = 400;
 
     const transformes = [
       // カメラを移動するとスプライトは逆向きに動くので反転
@@ -194,7 +297,9 @@ class Camera {
       // 回転中心をカメラ中心にする
       Mat4.translate(this.width / 2.0, this.height / 2.0, 0.0),
       // カメラの回転
-      Mat4.rotateZ(rad),
+      Mat4.rotateX(rad[0]),
+      Mat4.rotateY(rad[1]),
+      Mat4.rotateZ(rad[2]),
       // 中心座標を戻す
       Mat4.translate(-this.width / 2.0, -this.height / 2.0, 0.0),
       // 反転を戻す
@@ -202,27 +307,24 @@ class Camera {
       // スケール変換
       Mat4.scale(scale[0], scale[1], scale[2]),
       //クリップ座標系に変換
-      Mat4.scale(2.0 / this.width, 2.0 / this.height, 0.0),
+      Mat4.scale(2.0 / this.width, 2.0 / this.height, 2 / depth),
       Mat4.translate(-1.0, -1.0, 0.0),
       // Y軸のみ反転
       Mat4.scale(1.0, -1.0, 1.0),
     ];
 
     this.matrix = new Float32Array(Mat4.mulAll(transformes));
-    console.log(transformes);
-    console.log(this.matrix);
-    console.log(Mat1.mul4(this.matrix, [0, 0, 0, 1]));
-    console.log(Mat1.mul4(this.matrix, [100, 100, 0, 1]));
   }
 }
 
 const canvas = document.getElementById("canvas");
 let gl = null;
+let vertexColorAttributeLocation;
 let positionAttributeLocation;
 let matrixLocation;
 let camera;
 
-const planes = [];
+const primitives = [];
 
 function initialize() {
   gl = canvas.getContext("webgl");
@@ -235,17 +337,18 @@ function initialize() {
   gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
   camera = new Camera(gl.canvas.width, gl.canvas.height);
 
+  vertexColorAttributeLocation = gl.getAttribLocation(program, "a_color");
   positionAttributeLocation = gl.getAttribLocation(program, "a_position");
   matrixLocation = gl.getUniformLocation(program, "u_matrix");
 }
 
 function uploadVertex() {
-  const targetSprite = planes[0];
+  const targetSprite = primitives[0];
 
   /* --------------------------------------------------------------------------------- */
   /* positionBuffer に頂点データをセットする */
   const positionBuffer = gl.createBuffer();
-  const vertexes = new Float32Array(planes.map( plane => plane.vertexes ).flat());
+  const vertexes = new Float32Array(primitives.map( primitive => primitive.vertexes ).flat());
   gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
   gl.bufferData(
     gl.ARRAY_BUFFER,
@@ -254,10 +357,27 @@ function uploadVertex() {
   );
 
   /* --------------------------------------------------------------------------------- */
+  /* positionBuffer に頂点データをセットする */
+  const colorBuffer = gl.createBuffer();
+  const colors = new Uint8Array(primitives.map( primitive => primitive.vertexColors ).flat());
+  gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
+  gl.bufferData(
+    gl.ARRAY_BUFFER,
+    colors,
+    gl.STATIC_DRAW
+  );
+
+  /* --------------------------------------------------------------------------------- */
   /* 属性(a_position)にバッファ(positionBuffer)の内容を流し込む */
   gl.enableVertexAttribArray(positionAttributeLocation);
   gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
   gl.vertexAttribPointer(positionAttributeLocation, 3, gl.FLOAT, false, 0, 0);
+
+  /* --------------------------------------------------------------------------------- */
+  /* 属性(a_color)にバッファ(colorBuffer)の内容を流し込む */
+  gl.enableVertexAttribArray(vertexColorAttributeLocation);
+  gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
+  gl.vertexAttribPointer(vertexColorAttributeLocation, 4, gl.UNSIGNED_BYTE, false, 0, 0);
 }
 
 function render(camera) {
@@ -271,7 +391,7 @@ function render(camera) {
   gl.uniformMatrix4fv(matrixLocation, false, camera.matrix);
 
   /* 頂点ベクトルの始点（0）から n つぶんの頂点を利用してプリミティブを描画 */
-  gl.drawArrays(gl.TRIANGLES, 0, 6 * planes.length);
+  gl.drawArrays(gl.TRIANGLES, 0, primitives.reduce((acc, val) => acc + val.vertexes.length, 0) / 3);
 }
 
 function step() {
@@ -281,11 +401,11 @@ function step() {
 
 initialize();
 
-const plane = new Plane(0, 0, 0, 100, 100);
-plane.initialize(gl);
-planes.push(plane);
+const plane = new Plane(100, 100, 0, 100, 100);
+const cube = new Cube(100, 100, 0, 100, 100, 100);
+primitives.push(cube);
 
-camera.setTransformMatrix([0.0, 0.0, 0.0], 0.0, [1.0, 1.0, 1.0]);
+camera.setTransformMatrix([0.0, -100.0, 0.0], [30.0, 10.0, 0.0], [1.0, 1.0, 1.0]);
 
 uploadVertex();
 step();
